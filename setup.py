c225e3d184e4348b4044828bdce9308b616c71f8
Nasim Tabatabaei <nasim.taba2@gmail.com>
2016-12-18 23:23:42 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     version='1.2.0',
     packages=['standard_deviation',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     author='Nasim Tabatabaei, Nada Salem',
 )
